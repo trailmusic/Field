32db7723dbeff425703a279e313070b58cc7960e
trailmusic <grant@trail.wtf>
2025-10-03 16:35:51 -0700

    WO-12: remove duplicate createParameterLayout to avoid conflict with shared core; ParamLayout still compiled in field_core

--- Source/processor/PluginProcessor.cpp
+++ Source/processor/PluginProcessor.cpp
@@ -1,7 +1,1 @@
 #include "../shared/Core/PluginProcessor.cpp"
-#include "core/params/ParamLayout.h"
-
-juce::AudioProcessorValueTreeState::ParameterLayout MyPluginAudioProcessor::createParameterLayout()
-{
-    return field::params::makeParameterLayout();
-}
